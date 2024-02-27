06a159db585f95ba86d9b44723c3be27eace2db1
Faisal Tariq <faisaltariq183@gmail.com>
2024-02-26 16:29:17 -0800

    chore(configuration): add ESLint & Prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,23 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "avoid",
+  bracketSpacing: false,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
 };
 
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import { fontSans } from "~/components/ui/fonts";
-import { siteConfig } from "~/constants/config";
-import { cn } from "~/lib/utils";
-import "~/styles/globals.css";
+import {fontSans} from '~/components/ui/fonts'
+import {siteConfig} from '~/constants/config'
+import {cn} from '~/lib/utils'
+import '~/styles/globals.css'
 
 export const metadata = {
   title: siteConfig.title,
   description: siteConfig.description,
   icons: siteConfig.icons,
-};
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body
         className={cn(
-          "bg-background min-h-screen font-sans antialiased",
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable,
         )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
