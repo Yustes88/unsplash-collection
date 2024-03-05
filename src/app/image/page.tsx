6348cd67859093ccc8a8db95c9e5f22e0ac06519
Iuliia <iuliiaobr.io@gmail.com>
2024-03-05 21:44:32 +0700

    feature:added search result layout with mock image

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,7 +1,22 @@
+'use client'
 import React from 'react'
+import SearchInput from '~/components/ui/search-input'
+import {SearchResult} from '~/components/ui/search-result'
 
 const ImageSearchPage = () => {
-  return <div>ImageSearchPage</div>
+  return (
+    <>
+      <div className="relative">
+        <div className="gradient-image absolute top-0 h-24 w-full"></div>
+        <div className="absolute left-1/2 right-0 top-1/2 -translate-x-1/2 translate-y-full transform">
+          <SearchInput className={'bg-white'} />
+        </div>
+      </div>
+      <div className="container mb-20 mt-48">
+        <SearchResult />
+      </div>
+    </>
+  )
 }
 
 export default ImageSearchPage
