efd611a913b709c74b90fba5f7566c0b1d079c3b
Yustes88 <iuliiaobr.io@gmail.com>
2024-04-21 22:33:28 +0300

    feat:added google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,20 @@
-import {handlers} from 'auth'
+import {handlers} from '~/lib/auth'
+import GoogleProvider from 'next-auth/providers/google'
 
-// import { handlers } from "@/auth" // Referring to the auth.ts we just created
 export const {GET, POST} = handlers
+
+export const OPTION = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+}
