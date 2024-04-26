2ac6773fc28d197e954472f9ef76e3fa790b2eb8
Faisal Tariq <faisaltariq183@gmail.com>
2024-04-26 22:57:22 +0400

    feat(pages):  create-collection

--- src/app/user/create/collection/page.tsx
+++ src/app/user/create/collection/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Switch } from "~/components/ui/switch";
 
 const CreateCollection = () => {
-  return <div>CreateCollection</div>;
+  return (
+    <form className=" mx-auto my-6 max-w-xl rounded-md p-4 ">
+      <div className="mb-6 space-y-2">
+        <h1 className="text-[18px] font-bold italic">Create Collection</h1>
+        <p className="text-xs ">
+          Embark on your creative journey by crafting your own collection of
+          captivating images
+        </p>
+      </div>
+      <div className=" size-full  space-y-5">
+        <div className="w-full space-y-2">
+          <Label htmlFor="collection-name">Collection Name</Label>
+          <Input id="collection-name" />
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <Switch id="status" />
+          <Label htmlFor="status">Make it Public</Label>
+        </div>
+
+        <Button
+          size="sm"
+          variant="secondary"
+          className="w-full dark:bg-gray-800"
+        >
+          Create Collection
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default CreateCollection;
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,72 +1,72 @@
-import * as React from 'react'
-import {Slot} from '@radix-ui/react-slot'
-import {cva, type VariantProps} from 'class-variance-authority'
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import {cn} from '~/lib/utils'
+import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'size-8',
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "size-8",
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: "default",
+      size: "default",
     },
   },
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-  icon?: React.ReactNode
+  asChild?: boolean;
+  icon?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({className, variant, size, icon, asChild = false, ...props}, ref) => {
-    const Comp = asChild ? Slot : 'button'
+  ({ className, variant, size, icon, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
 
     if (asChild) {
       return (
         <Comp
-          className={cn(buttonVariants({variant, size, className}))}
+          className={cn(buttonVariants({ variant, size, className }))}
           ref={ref}
           {...props}
         />
-      )
+      );
     }
 
     return (
       <Comp
-        className={cn(buttonVariants({variant, size, className}))}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
         {icon && <span className="mr-2">{icon}</span>}
         {props.children}
       </Comp>
-    )
+    );
   },
-)
-Button.displayName = 'Button'
+);
+Button.displayName = "Button";
 
-export {Button, buttonVariants}
+export { Button, buttonVariants };
